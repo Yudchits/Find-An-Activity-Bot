834efe01b52e70bab453d9b2724926e923640660
Yudchits <pasha.01.03.2003@mail.ru>
2023-07-01 23:12:32 +0300

    created the class for getting information from web site

---
+++ src/main/java/by/yudchits/when_you_are_bored/client/Activity.java
@@ -0,0 +1,33 @@
+package by.yudchits.when_you_are_bored.client;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class Activity {
+
+    @Autowired
+    private OkHttpClient client;
+
+    @Value("${activity.url}")
+    private String URL;
+
+    public String getActivity(){
+        var request = new Request.Builder()
+                .url(URL)
+                .build();
+
+        try(var respond = client.newCall(request).execute()){
+            var body = respond.body();
+
+            return body == null ? null : body.string();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/by/yudchits/when_you_are_bored/configuration/BotConfig.java
+++ src/main/java/by/yudchits/when_you_are_bored/configuration/BotConfig.java
@@ -1,7 +1,9 @@
 package by.yudchits.when_you_are_bored.configuration;
 
 import lombok.Data;
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -13,4 +15,9 @@ public class BotConfig {
 
     @Value("${bot.token}")
     private String botToken;
+
+    @Bean
+    public OkHttpClient okHttpClient(){
+        return new OkHttpClient();
+    }
 }
