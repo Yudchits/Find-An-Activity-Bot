8c16706636ef5d12c28872c68393d02f16f961e2
Yudchits <pasha.01.03.2003@mail.ru>
2023-07-01 22:42:39 +0300

    initialized the project and added dependencies

---
+++ src/main/java/by/yudchits/when_you_are_bored/WhenYouAreBoredApplication.java
@@ -0,0 +1,13 @@
+package by.yudchits.when_you_are_bored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhenYouAreBoredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhenYouAreBoredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/yudchits/when_you_are_bored/WhenYouAreBoredApplicationTests.java
@@ -0,0 +1,13 @@
+package by.yudchits.when_you_are_bored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhenYouAreBoredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
